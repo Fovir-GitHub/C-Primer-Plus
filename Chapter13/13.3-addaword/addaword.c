bea7c3a36f93238d915ef8db00444b8d36b2469e
Fovir <fovir@disroot.org>
2024-07-14 21:37:19 +0800

    add 13.3 addaword.c

---
+++ Chapter13/13.3-addaword/addaword.c
@@ -0,0 +1,36 @@
+// use fprintf(), fscanf(), and rewind()
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 41
+
+int main(void)
+{
+	FILE* fp;
+	char   words[MAX];
+
+	if ((fp = fopen("wordy.txt", "a+")) == NULL)
+	{
+		fprintf(stdout, "Can't open wordy file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	puts("Enter word to add to the file; press the #");
+	puts("key at the beginning of a line to terminate.");
+
+	while ((fscanf(stdin, "%40s", words) == 1) && (words[0] != '#'))
+		fprintf(fp, "%s\n", words);
+
+	puts("File contents:");
+	rewind(fp); // go back to the beginning of file
+
+	while (fscanf(fp, "%s", words) == 1)
+		puts(words);
+	puts("Done!");
+	if (fclose(fp))
+		fprintf(stderr, "Error closing file\n");
+
+	return 0;
+}
