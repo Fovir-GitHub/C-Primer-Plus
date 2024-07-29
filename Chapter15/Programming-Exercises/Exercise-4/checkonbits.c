ebd6c0792a1f7a4660a45f1c530970b5cc5b8438
Fovir <fovir@disroot.org>
2024-07-29 16:11:45 +0800

    finish checkonbits.c

---
+++ Chapter15/Programming-Exercises/Exercise-4/checkonbits.c
@@ -0,0 +1,23 @@
+// Write a function that takes two int arguments : a value and a bit position.
+// Have the function return 1 if that particular bit position is 1,
+// and have it return 0 otherwise.Test the function in a program.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool thePositionisOn(int n, int position);
+
+int main(void)
+{
+	int n, pos;
+
+	while (scanf("%d %d", &n, &pos) == 2)
+		printf("%d\n", thePositionisOn(n, pos));
+
+	return 0;
+}
+
+bool thePositionisOn(int n, int position)
+{
+	return (n & (1 << position));
+}
