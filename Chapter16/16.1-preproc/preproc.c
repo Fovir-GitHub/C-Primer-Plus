c93109612a6e264729eab6252191fa7f1c3e6ab4
Fovir <fovir@disroot.org>
2024-07-30 12:51:29 +0800

    add preproc.c

---
+++ Chapter16/16.1-preproc/preproc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#define TWO 2
+#define OW "Consistency is the last refuge of the unimagina\
+tive. - Oscar Wilde"
+
+#define FOUR TWO*TWO
+#define PX printf("X is %d\n",x)
+
+int main(void)
+{
+	int x = TWO;
+	PX;
+	x = FOUR;
+	PX;
+	printf("%s\n", OW);
+	printf("TWO: OW\n");
+
+	return 0;
+}
