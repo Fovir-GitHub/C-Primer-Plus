6d4e2f3ba9db063deaf1417037a9d0871d79d66e
Fovir <fovir@disroot.org>
2024-07-30 13:43:24 +0800

    add names_st.h names_st.c useheader.c

---
+++ Chapter16/16.6-names_st/names_st.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include"names_st.h"
+
+void get_names(names * pn)
+{
+	printf("Please enter your first name: ");
+	s_gets(pn->first, SLEN);
+
+	printf("Please enter your last name: ");
+	s_gets(pn->last, SLEN);
+
+	return;
+}
+
+void show_names(const names * pn)
+{
+	printf("%s %s", pn->first, pn->last);
+
+	return;
+}
+
+char * s_gets(char * st, int n)
+{
+	char * ret_val;
+	char * find;
+
+	ret_val = fgets(st, n, stdin);
+
+	if (ret_val)
+	{
+		find = strchr(ret_val, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+
+	return ret_val;
+}
